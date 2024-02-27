37e3aa1c130a486efacdfe7ef175b87bb8d3b5d3
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-02-26 20:06:48 -0500

    feat: add buttons to page component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      Dahlia 
+      Dahlia
+      <div className=" flex gap-4 py-4">
+        <Button variant="outline">Button</Button>
+        <Button>Button</Button>
+        <Button variant="destructive">Button</Button>
+        <Button variant="ghost">Button</Button>
+        <Button variant="link">Button</Button>
+      </div>
     </main>
   );
 }
