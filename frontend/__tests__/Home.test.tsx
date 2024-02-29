70a7503880555284e064aacfb2701a4592513e68
Ricky Chon <rickychon99@gmail.com>
2024-02-28 20:15:45 -0500

    ci(frontend): setup lint workflow (#25)
    * ci(frontend): setup lint workflow
    
    * ci(frontend-lint): set default working directory
    
    * chore(lint): run eslint & prettier
    
    * chore(deps): update workflow node version to 20
    
    v16 and v18 are deprecated
    
    * chore(lint): make ESLint compatible with Jest
    
    Turned off `no-var-requires` rule

--- frontend/__tests__/Home.test.tsx
+++ frontend/__tests__/Home.test.tsx
@@ -1,17 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Home from "../src/app/page";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../src/app/page';
 
-describe("Home Component", () => {
-  test("renders Home component without crashing", () => {
+describe('Home Component', () => {
+  test('renders Home component without crashing', () => {
     render(<Home />);
     const linkElement = screen.getByText(/Dahlia/i);
     expect(linkElement).toBeInTheDocument();
   });
-  test("renders buttons with correct variants", () => {
+  test('renders buttons with correct variants', () => {
     render(<Home />);
 
-    const buttons = screen.getAllByRole("button");
+    const buttons = screen.getAllByRole('button');
     expect(buttons.length).toBe(5);
   });
 });
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Dahlia",
-  description: "Concert Parking App",
+  title: 'Dahlia',
+  description: 'Concert Parking App'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
