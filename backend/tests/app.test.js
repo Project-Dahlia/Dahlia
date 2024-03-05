def27df7794b7fc7aa62fe2e998c671f6f5959f0
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-03-05 09:14:48 -0500

    test: update test definition

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
-const app = require('../app');
+const express = require('express');
+const homeRouter = require('../src/routes/home-route');
 
-describe('Test the root path', () => {
-  test('It should respond with the GET method', async () => {
+const app = express();
+app.use('/', homeRouter);
+
+describe('Test the home path', () => {
+  test('It should response the GET method', async () => {
     const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toEqual('Hello Parking User');
+    expect(response.statusCode).toBe(201);
+    expect(response.text).toBe('Home route is working!');
   });
 });
