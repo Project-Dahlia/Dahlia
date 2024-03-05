1cacad3f6fdc766690a316bd22b6cbf47fe631b3
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-03-05 09:59:40 -0500

    chore: update test definition message

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/', homeRouter);
 
 describe('Test the home path', () => {
-  test('It should response the GET method', async () => {
+  test('It should respond with 200 OK', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Home route is working!');
