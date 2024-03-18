44a7b1a5a607d7bf2b391183e54425821b7616a0
Jay Abegglen <jsabegglen96@gmail.com>
2024-03-18 15:47:58 -0700

    chore(frontend): update prettier issue

--- packages/frontend/src/app/map.tsx
+++ packages/frontend/src/app/map.tsx
@@ -3,77 +3,73 @@
 import * as L from 'leaflet';
 
 class Map {
-    private map: L.Map | undefined;
-    private zoom: number =13;
-    private mapEvent: L.Events | undefined;
-    private marker: L.Marker[] = [];
+  private map: L.Map | undefined;
+  private zoom: number = 13;
+  private mapEvent: L.Events | undefined;
+  private marker: L.Marker[] = [];
 
-    //get user position when run TODO -- and event?
-    constructor() {
-        this._getPosition();
-    
-        // collect data from storage api TODO
+  //get user position when run TODO -- and event?
+  constructor() {
+    this._getPosition();
 
-    }
+    // collect data from storage api TODO
+  }
 
-    _getPosition() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                this._loadMap.bind(this),
-                function () {
-                    alert('Could not get your position');
-                }
-            );
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
         }
+      );
     }
+  }
 
-    _loadMap(position: GeolocationPosition) {
-        const { latitude } = position.coords;
-        const { longitude } = position.coords;
-        //To test in Toronto:
-        // console.log(`https://www.google.com/maps/@${latitude},${longitude}?entry=ttu`);
-        
-        const coords: L.LatLngTuple = [latitude, longitude];
-        // Sets zoom level and map center
-        this.map = L.map('map').setView(coords, this.zoom);
+  _loadMap(position: GeolocationPosition) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    //To test in Toronto:
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}?entry=ttu`);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(this.map);
+    const coords: L.LatLngTuple = [latitude, longitude];
+    // Sets zoom level and map center
+    this.map = L.map('map').setView(coords, this.zoom);
 
-        //From Leaflet to click on map
-        this.map.on('click', this._mapClick.bind(this));
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
 
-        //render markers to map now map is loaded:
-        // TODO
-    }
+    //From Leaflet to click on map
+    this.map.on('click', this._mapClick.bind(this));
 
-    _renderEventData(){
-        //TODO
-    }
+    //render markers to map now map is loaded:
+    // TODO
+  }
 
-    _renderParkingData(){
-        //TODO
-    }
+  _renderEventData() {
+    //TODO
+  }
 
-    _moveToPopup(e: L.LeafletMouseEvent){
-        //TODO
-        //move to the clicked popup
-        // show form for change route / parking information here -- tbd
-        
-    }
+  _renderParkingData() {
+    //TODO
+  }
 
-    _mapClick(e: L.LeafletMouseEvent) {
-        //TODO
-    }
-    
-    _setMongoData(){
-        //TODO
-    }
+  _moveToPopup(e: L.LeafletMouseEvent) {
+    //TODO
+    //move to the clicked popup
+    // show form for change route / parking information here -- tbd
+  }
 
-    _getMongoData(){
-        //TODO
-    }
+  _mapClick(e: L.LeafletMouseEvent) {
+    //TODO
+  }
 
+  _setMongoData() {
+    //TODO
+  }
 
+  _getMongoData() {
+    //TODO
+  }
 }
