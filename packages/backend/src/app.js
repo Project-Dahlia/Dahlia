1b3514400a70d0ce00e9857d025d72dc1ec8048a
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-04-11 18:53:46 -0400

    chore: add cors to app

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -4,12 +4,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
 const swaggerDocs = require('./swagger-ui/swagger');
 
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 //v1 api routes
 app.use('/api/v1', routes);
