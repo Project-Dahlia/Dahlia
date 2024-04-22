107647d827eecc114fdfcd251a5d4ddcd3322d10
xamdoo <75206869+xamdoo@users.noreply.github.com>
2024-04-22 22:28:42 +0300

    feat: conditional navbar rendering for login/register button

--- packages/frontend/src/components/site-header.tsx
+++ packages/frontend/src/components/site-header.tsx
@@ -1,34 +1,49 @@
+'use client';
 import { Button } from './ui/button';
 import { MainNav } from './main-nav';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function Header() {
+  const pathName = usePathname();
+
+  // Conditional rendering of the auth buttons
+  const showLoginButton = pathName !== '/login';
+  const showRegisterButton = pathName !== '/register';
+
   return (
     <header
-      className="sticky top-0 h-14 bg-white px-2 backdrop-blur-xl sm:flex sm:justify-between"
+      className="sticky top-0 h-14 bg-white backdrop-blur-2xl sm:flex sm:justify-between"
       data-testid="header-container"
     >
-      <div className="container flex items-center justify-around">
+      <div className="container flex items-center justify-between">
         <MainNav />
-        <div className="flex justify-end">
-          <nav className="mx-4 flex items-center gap-6 py-2">
+        {/* <div className="flex flex-1 justify-end border border-purple-600"> */}
+        <nav className="mx-4 flex items-center gap-6 py-2">
+          {showLoginButton && (
             <Button
+              asChild
               variant="outline"
               size="sm"
               className="border-gray-400 bg-white px-4 font-bold"
               aria-label="Login"
             >
-              Login
+              <Link href="/login">Login</Link>
             </Button>
+          )}
+          {showRegisterButton && (
             <Button
+              asChild
               variant="default"
               className="px-4 font-bold"
               size="sm"
               aria-label="Register"
             >
-              Register
+              <Link href="/register">Register</Link>
             </Button>
-          </nav>
-        </div>
+          )}
+        </nav>
+        {/* </div> */}
       </div>
     </header>
   );
