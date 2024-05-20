c80fdd9f488ce630bd735327dd1a448bc70c25e8
Jay Abegglen <jsabegglen96@gmail.com>
2024-05-20 12:29:11 -0700

    chore(frontend): merging

---
+++ packages/frontend/src/components/map/Map.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
+
+const position: [number, number] = [51.505, -0.09];
+
+const MyMap: React.FC = () => {
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MyMap;
