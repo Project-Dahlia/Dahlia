ef1ebbe229e3b747c2fbeb927a320dabf1852fec
Jay Abegglen <jsabegglen96@gmail.com>
2024-08-21 21:27:54 -0700

    feat(backend): add stub config/db plus parser

--- packages/backend/src/seeders/loadParkingData.js
+++ packages/backend/src/seeders/loadParkingData.js
@@ -1,21 +1,42 @@
 const fs = require('fs');
 const ParkingSpot = require('../models/ParkingSpot');
+const sequelize = require('../models/p-database');
 
-fs.readFile('path/to/your/file.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  const jsonData = JSON.parse(data);
-  const carparkArray = jsonData.carpark;
+// Ensure tables are created or updated before seeding data
+async function seedDatabase() {
+  try {
+    await sequelize.sync();
+    console.log('Database & tables ready for seeding!');
 
-  // Insert the data in batches
-  const batchSize = 1000; // Adjust batch size as needed
-  for (let i = 0; i < carparkArray.length; i += batchSize) {
-    const batch = carparkArray.slice(i, i + batchSize);
-    ParkingSpot.bulkCreate(batch)
-      .then(() =>
-        console.log(`Batch ${i / batchSize + 1} inserted successfully`)
-      )
-      .catch((err) =>
-        console.error(`Error inserting batch ${i / batchSize + 1}:`, err)
-      );
+    fs.readFile(
+      '../data/toronto-parking-2019.json',
+      'utf8',
+      async (err, data) => {
+        if (err) throw err;
+        const jsonData = JSON.parse(data);
+        const carparkArray = jsonData.carpark;
+
+        const batchSize = 1000;
+        for (let i = 0; i < carparkArray.length; i += batchSize) {
+          const batch = carparkArray.slice(i, i + batchSize);
+
+          try {
+            await ParkingSpot.bulkCreate(batch);
+            console.log(
+              `Batch ${Math.floor(i / batchSize) + 1} inserted successfully`
+            );
+          } catch (err) {
+            console.error(
+              `Error inserting batch ${Math.floor(i / batchSize) + 1}:`,
+              err
+            );
+          }
+        }
+      }
+    );
+  } catch (error) {
+    console.error('Error syncing database:', error);
   }
-});
+}
+
+seedDatabase();
