7b9067f8df7dbc729a160b9a81772e30e651f586
Jay Abegglen <jsabegglen96@gmail.com>
2024-09-18 18:29:34 -0700

    fix(frontend): add search tsx stub

---
+++ packages/frontend/src/components/map/search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input } from '../ui/input';
+
+const MapSearch = () => {
+  return (
+    <div className="">
+      <Input placeholder="Search" />
+    </div>
+  );
+};
+
+export default MapSearch;
