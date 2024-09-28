2e2b9152f03a019f2990c58f460895a49fe5924f
xamdoo <75206869+xamdoo@users.noreply.github.com>
2024-09-28 12:05:42 +0300

    refactor: pass data to make it dyanmic

--- packages/frontend/src/components/common/parking-card/parkcard-wrapper.tsx
+++ packages/frontend/src/components/common/parking-card/parkcard-wrapper.tsx
@@ -2,26 +2,39 @@ import React, { useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ParkCard } from './parkcard';
 import { Icons } from '../icons';
+import { cn } from '@/lib/utils';
 
 export function ParkingCardWrapper() {
   const [activeTab, setActiveTab] = useState<'distance' | 'price'>('distance');
 
   return (
-    <div className="absolute mx-2 mt-2 w-[270px]">
+    <div className="absolute mx-2 mt-1 ">
       <Tabs
         defaultValue="distance"
         onValueChange={(value) => setActiveTab(value as 'distance' | 'price')}
         className=""
       >
-        <TabsList className="h-10 w-full">
-          <TabsTrigger value="distance" className="w-full">
+        <TabsList className="h-10 w-full bg-gray-100">
+          <TabsTrigger
+            value="distance"
+            className={cn(
+              'w-full rounded-lg',
+              'data-[state=active]:bg-white data-[state=active]:text-black'
+            )}
+          >
             <Icons.parking
               active={activeTab === 'distance'}
               className="mr-2 h-6 w-6"
             />
             {'Distance'}
           </TabsTrigger>
-          <TabsTrigger value="price" className="w-full">
+          <TabsTrigger
+            value="price"
+            className={cn(
+              'w-full rounded-lg',
+              'data-[state=active]:bg-white data-[state=active]:text-black'
+            )}
+          >
             <Icons.price
               active={activeTab === 'price'}
               className="mr-2 h-6 w-6"
@@ -29,11 +42,43 @@ export function ParkingCardWrapper() {
             {'Price'}
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="distance" className="">
-          <ParkCard />
+        <TabsContent value="distance">
+          <ParkCard
+            title="City Center Parking"
+            spaces={15}
+            price={10}
+            duration="1 hour"
+            timeToDestination="5"
+          />
+          <ParkCard
+            title="Downtown Parking"
+            spaces={20}
+            price={20}
+            duration="2 hours"
+            timeToDestination="10"
+            footerIcons={[
+              <Icons.CreditCard key="credit-card" />,
+              <Icons.Bike key="bike" />,
+              <Icons.Car key="car" />,
+              <Icons.Zap key="zap" />
+            ]}
+          />
         </TabsContent>
+
         <TabsContent value="price">
-          <ParkCard />
+          <ParkCard
+            title="Downtown Parking"
+            spaces={20}
+            price={20}
+            duration="2 hours"
+            timeToDestination="10"
+            footerIcons={[
+              <Icons.CreditCard key="credit-card" />,
+              <Icons.Bike key="bike" />,
+              <Icons.Car key="car" />,
+              <Icons.Zap key="zap" />
+            ]}
+          />
         </TabsContent>
       </Tabs>
     </div>
--- packages/frontend/src/components/common/parking-card/parkcard.tsx
+++ packages/frontend/src/components/common/parking-card/parkcard.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import {
   Card,
   CardContent,
@@ -9,36 +8,66 @@ import {
 } from '@/components/ui/card';
 import { Icons } from '../icons';
 
-export function ParkCard() {
+interface ParkCardProps {
+  title: string;
+  spaces: number;
+  price: number;
+  duration: string;
+  timeToDestination: string;
+  footerIcons?: Array<React.ReactNode>;
+}
+
+export function ParkCard({
+  title,
+  spaces,
+  price,
+  duration,
+  timeToDestination,
+  footerIcons
+}: ParkCardProps) {
   return (
-    <Card className="mb-2 rounded-md  shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
+    <Card className="mb-2 rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
       <CardHeader className="m-0 flex flex-row items-center justify-between rounded-t-md bg-sky-100 p-0">
-        <CardTitle className="p-1 text-sm font-normal">
-          Downtown Parking
-        </CardTitle>
-        <span className="p-1 text-xs/[0.01px]">20 spaces</span>
+        <CardTitle className="p-1 text-sm font-normal">{title}</CardTitle>
+        <span className="p-1 text-xs/[0.01px]">{spaces} spaces</span>
       </CardHeader>
-      <CardContent className="mx-5 flex justify-between gap-10 p-0">
-        <div className="flex items-center">
-          <Icons.price className=" h-7 w-7 text-gray-200" />
-          <span className="mt-3 flex flex-col text-3xl font-semibold leading-none">
-            20
-            <span className="text-xs font-normal text-gray-200">2 hours</span>
+      <CardContent className="flex flex-row items-center justify-between space-x-8 py-0">
+        <div className="flex items-baseline">
+          <Icons.price className="h-7 w-7 text-gray-200" />
+          <span className="mt-2 flex w-full flex-col text-3xl leading-none">
+            {price}
+            <small className="text-xs font-normal text-gray-200">
+              {duration}
+            </small>
           </span>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-baseline space-x-1">
           <Icons.clock className="h-7 w-7 text-gray-200" />
-          <span className="mt-3 text-3xl font-semibold leading-none">
-            10
-            <span className="text-xs font-normal text-gray-200 ">
-              {' '}
-              minutes to destination
+          <span className="mt-2 flex w-full flex-col">
+            <span className="flex items-baseline ">
+              <span className="text-3xl leading-none">{timeToDestination}</span>
+              <small className="text-xs font-normal text-gray-200">
+                minutes
+              </small>
             </span>
+            <small className="text-xs font-normal text-gray-200">
+              to destination
+            </small>
           </span>
         </div>
       </CardContent>
-      <hr className="" />
-      <CardFooter className="flex justify-between"></CardFooter>
+      <hr />
+      <CardFooter className="mx-1 flex space-x-2 p-1">
+        {footerIcons && footerIcons.length > 0 ? (
+          footerIcons.map((icon, index) => (
+            <span key={index} className="h-5 w-5">
+              {icon}
+            </span>
+          ))
+        ) : (
+          <div className="h-4 w-full" />
+        )}
+      </CardFooter>
     </Card>
   );
 }
