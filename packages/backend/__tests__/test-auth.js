f6a678a2a8b2dcf0d963a8f9b3b9dc7e712f6f50
Jay Abegglen <jsabegglen96@gmail.com>
2024-10-09 23:52:40 -0700

    fix(backend): resolving jest issues

--- packages/backend/__tests__/test-auth.js
+++ packages/backend/__tests__/test-auth.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../src/config/database');
 
 const Test = sequelize.define('Test', {
   firstName: {
