cb0acb46725161cf056eaa7278c9fa97d52acfda
Kelli Thoresen <tkelli@pdx.edu>
2024-10-29 23:52:14 -0700

    resolve 404 error on parking page

---
+++ packages/frontend/src/app/parking/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import { useAuth } from '@/lib/hooks/use-auth'; // Adjust the import path as needed
+
+const Parking = () => {
+  const { isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+
+  return (
+    <div className="parking-container container my-10 flex flex-col">
+        <h1 className="font-subheading text-2xl font-semibold tracking-tight" style={{marginLeft: 288+'px', marginTop: 50+'px'}}>
+          Parking Page
+        </h1>
+    </div>
+  );
+};
+
+export default Parking;
